93fe1453f737c77df5ca3c7d4a2e94d57fd8f3c4
Aditya Bisht <adityabisht64@gmail.com>
2024-03-15 09:42:39 +0530

    feat: change to nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.node$/,
+      loader: "nextjs-node-loader",
+    });
+
+    return config;
+  },
+};
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,207 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+import React, { useState, useEffect } from 'react';
+import { genCircomAllstr } from '../lib/gen_circom';
+import { applyMinDfa, regexToDfa } from '../lib/regex';
+// import compiler from '@zk-email/zk-regex-compiler';
+
+const PageComponent: React.FC = () => {
+    const [regex, setRegex] = useState<string>("a=(b|c)+ d=e");
+    const [inputs, setInputs] = useState<string[]>(["a=bccccccccc d=e", "", ""]);
+    const [minDfa, setMinDfa] = useState<any[]>([]);
+    const [reveals, setReveals] = useState<string[][]>([[]]);
+    const [parsedInputs, setParsedInputs] = useState<any[]>([]);
+    const [circom, setCircom] = useState<string>("");
+    const [currentReveal, setCurrentReveal] = useState<number>(0);
+
+    const validateRegex = (regex: string | RegExp) => {
+        try {
+            new RegExp(regex);
+            // console.log("Testing ", compiler);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    };
+
+    const validateInput = (input: string, regex: string | RegExp) => {
+        if (!validateRegex(regex)) {
+            return false;
+        }
+        const regexObj = new RegExp(regex);
+        return !input || regexObj.test(input);
+    };
+
+    const test = () => {
+        if (!validateRegex(regex)) {
+            return;
+        }
+        if (inputs.some(input => !validateInput(input, regex))) {
+            return;
+        }
+        const dfa = regexToDfa(regex);
+        setMinDfa(dfa);
+        let results = [];
+        let replacedInputs = inputs.map(s => s.replaceAll("\n", "\r\n"));
+        for (let input of replacedInputs) {
+            if (input === "") {
+                results.push([]);
+                continue;
+            }
+            let result = applyMinDfa(dfa, input);
+            results.push(result.length > 0 ? result : []);
+        }
+        setParsedInputs(results);
+    };
+
+    const displayChar = (c: string) => {
+        if (c === "\r") {
+            return "\\r";
+        }
+        if (c === "\n") {
+            return "\\n";
+        }
+        return c;
+    };
+
+    const generate = () => {
+        let r = reveals.map(r1 => r1.map(r2 => r2.split(",")));
+        const circomStr = genCircomAllstr(minDfa, "Test", r, regex);
+        setCircom(circomStr);
+    };
+
+    const selectRevealState = (state: string[]) => {
+        const reveal = [...reveals[currentReveal]];
+        const transition = state[0] + "," + state[1];
+        const index = reveal.indexOf(transition);
+        if (index !== -1) {
+            reveal.splice(index, 1);
+        } else {
+            reveal.push(transition);
+        }
+        // Update the reveals array
+        const newReveals = [...reveals];
+        newReveals[currentReveal] = reveal;
+        setReveals(newReveals);
+    };
+
+    const removeReveal = (i: number) => {
+        const newReveals = reveals.length === 1 ? [[]] : reveals.filter((_, index) => index !== i);
+        setReveals(newReveals);
+        setCurrentReveal(newReveals.length - 1);
+    };
+
+    const stateSelected = (state: string[], reveals: string[][], currentReveal: number) => {
+        const reveal = reveals[currentReveal];
+        return reveal.includes(state[0] + "," + state[1]);
+    };
+
+    const newReveal = () => {
+        setReveals([...reveals, []]);
+        setCurrentReveal(reveals.length);
+    };
+
+    return (
+          <div className="h-auto w-full text-primary-content mb-16 mt-32 text-white">
+            <div className="h-auto overflow-y-scroll w-full flex items-center justify-center">
+                <div className="w-5/6">
+                <form className="w-full">
+                    <div className="form-control w-full mb-4">
+                        <label htmlFor="regex" className="label" id="regexpattern">
+                        <span className="label-text">Regex Pattern</span>
+                        </label>
+                        <input
+                        id="regex"
+                        type="text"
+                        className={`bg-transparent border border-gray-500 focus:border-gray-300 rounded-md py-2 px-4 focus:outline-none w-full ${!validateRegex(regex) ? 'input-error' : ''}`}
+                        value={regex}
+                        placeholder="(h|H)ello (w|W)orld"
+                        onChange={(e) => setRegex(e.target.value)}
+                        />
+                    </div>
+                    {Array.from({ length: 3 }, (_, index) => (
+                        <div className="form-control w-full mb-4" key={index}>
+                        <label htmlFor={`input${index + 1}`} className="label">
+                            <span className="label-text">Input {index + 1}</span>
+                        </label>
+                        <textarea
+                            id={`input${index + 1}`}
+                            className={`bg-transparent border border-gray-500 focus:border-gray-300 rounded-md py-2 px-4 focus:outline-none w-full ${!validateInput(inputs[index], regex) ? 'input-error' : ''}`}
+                            value={inputs[index]}
+                            placeholder=""
+                            onChange={(e) => {
+                                const newInputs = [...inputs];
+                                newInputs[index] = e.target.value;
+                                setInputs(newInputs);
+                            }}
+                        />
+                        </div>
+                    ))}
+                    <button className="btn mt-2 w-full bg-gray-600 rounded-md py-2 px-4" onClick={(e) => { e.preventDefault(); test(); }}>Continue</button>
+                    </form>
+                    {parsedInputs.length > 0 && (
+                        <div className="text-left w-full mt-4">
+                        Click the states below to select the characters you want to reveal or export.
+                        <br />
+                        <br />
+                        {parsedInputs.map((parsedInput, i) => (
+                            parsedInput.length > 0 && (
+                            <div key={i}>
+                                <div className="text-lg mb-2">
+                                Input {i + 1}: {parsedInput.length > 0 ? "Accepted" : "Rejected"}
+                                </div>
+                                {parsedInput.map((states: any[], index: React.Key | null | undefined) => (
+                                <div key={index}>
+                                    <table className="table text-center w-full">
+                                    <thead>
+                                        <tr className="border-b-stone-300">
+                                        {states.map((state, stateIndex) => (
+                                            <th key={stateIndex} className="p-0 w-5 cursor-pointer" onClick={() => selectRevealState(state)}>
+                                            {state[1]}
+                                            </th>
+                                        ))}
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr className="border-b-stone-300">
+                                        {states.map((state, stateIndex) => (
+                                            <td key={stateIndex} className={`p-0 w-5 cursor-pointer ${stateSelected(state, reveals, currentReveal) ? 'bg-yellow-50' : ''}`} onClick={() => selectRevealState(state)}>
+                                            {displayChar(state[2])}
+                                            </td>
+                                        ))}
+                                        </tr>
+                                    </tbody>
+                                    </table>
+                                    <br />
+                                </div>
+                                ))}
+                            </div>
+                            )
+                        ))}
+                        <div className="mt-10 w-full">
+                            <div className="text-xl">
+                            Selected Reveal Groups
+                            </div>
+                            <div className="flex flex-col">
+                            {reveals.map((reveal, i) => (
+                                <div key={i} className="flex flex-row items-center">
+                                <button className="btn btn-sm btn-circle btn-ghost text-red-500" onClick={() => removeReveal(i)}>✕</button>
+                                <div className="mr-4 cursor-pointer" onClick={() => setCurrentReveal(i)} style={{ fontWeight: currentReveal === i ? 'bold' : 'normal' }}>
+                                    Reveal #{i}
+                                </div>
+                                <div>{JSON.stringify(reveal)}</div>
+                                </div>
+                            ))}
+                            <button className="btn mt-4 w-1/3" onClick={newReveal}>+ Add New Reveal Group</button>
+                            </div>
+                        </div>
+                        <button className="btn mt-4 w-full" onClick={generate}>Generate</button>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
   );
-}
+};
+
+export default PageComponent;
