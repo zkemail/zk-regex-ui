9051209938b21445367eaff1b0d8ed729d27b8f1
Aditya Bisht <adityabisht64@gmail.com>
2024-03-15 09:49:39 +0530

    chore: rebase

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/routes/**/*.{svelte,js,ts}'],
-  plugins: [require("@tailwindcss/typography"), require('daisyui')],
-  theme: {
-    extend: {},
-  }
-}
